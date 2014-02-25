336018f037b1c8b70c4682fd6a2b2eb3073e961d
Ryan Brown <ryan@ryanleebrown.com>
2014-02-24 18:20:00 -0800

    add -global and -unglobal and a README

---
+++ README.md
@@ -0,0 +1,65 @@
+# gospace
+
+To switch to a different workspace, specify the path to the workspace.
+
+```
+$ gospace /foo/bar
+```
+
+To view the current active workspace, leave the path out.
+
+```
+$ gospace
+/foo/bar
+```
+
+To make it so that no workspace is active, use the `-clear` flag.
+
+```
+$ gospace -clear
+```
+
+To make a binary accessible from every workspace, use the `-global` flag and specify the path to the binary.
+
+```
+$ gospace -global /foo/bar/baz
+```
+
+To un-global a binary, use the `-unglobal` flag.
+
+```
+$ gospace -unglobal baz
+```
+
+## Install
+
+First, install it into one of your workspaces.
+
+```
+$ go get github.com/rynlbrwn/gospace
+```
+
+Now make `gospace` accessible from every workspace.
+
+```
+$ gospace -global gospace
+```
+
+Now modify your `$GOPATH` to point to `~/.gospace`. For example, you could put this in `~/.profile`:
+
+```
+export GOPATH=~/.gospace
+export PATH=$PATH:$GOPATH/bin
+```
+
+And finally set your current workspace:
+
+```
+$ gospace /foo/bar
+```
+
+## How Does It Work?
+
+The file at `~/.gospace` is a symlink to the currently active workspace. When you switch workspaces, the symlink is overwritten to point to the workspace you specified.
+
+The `-global` flag creates a symlink in `/usr/local/bin` to the binary in your current workspace (and `--unglobal` deletes it)
--- main.go
+++ main.go
@@ -4,66 +4,147 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
-var clearFlag = flag.Bool("clear", false, "make it so that no space is active")
+var (
+	clear    = flag.Bool("clear", false, "make it so that no space is active")
+	global   = flag.String("global", "", "make a binary accessible from every workspace")
+	unglobal = flag.String("unglobal", "", "undo what -global does")
+)
 
 func main() {
-	flag.Usage = func() {
-		fmt.Printf(`gospace is a tool for changing between Go workspaces. Just set your
-$GOPATH to ~/.gospace, which will be a symlink to your current
-workspace. gospace simply points the symlink to the path that you
-specifiy as an argument. Leave out the path to see the current one.
-
-Usage: %s [<path>]`, os.Args[0])
-		fmt.Println()
-		flag.PrintDefaults()
-		fmt.Println()
-	}
-	flag.Parse()
-	if *clearFlag {
-		clear()
-		get()
-		return
-	}
+	handleFlags()
 	switch len(os.Args) {
 	case 1:
-		get()
+		dst, err := readLink(spacefile())
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		if dst != "" {
+			fmt.Println(dst)
+		} else {
+			fmt.Println("no active workspace")
+		}
 	case 2:
-		set(os.Args[1])
+		dst := os.Args[1]
+		link := spacefile()
+		err := setLink(link, dst)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("%s -> %s\n", link, dst)
 	default:
-		fmt.Printf("Usage: %s [<path>]", os.Args[0])
+		printUsage()
 	}
 }
 
-func get() {
-	path, err := os.Readlink(spacefile())
-	if err != nil {
-		if !os.IsNotExist(err) {
-			fmt.Println(err)
+func printUsage() {
+	fmt.Printf(`gospace is a Go workspace switcher.
+
+More info: http://github.com/rynlbrwn/gospace
+
+Usage: %s [-clear|-global|-unglobal] [<path>]`, os.Args[0])
+	fmt.Println()
+	flag.PrintDefaults()
+	fmt.Println()
+}
+
+func handleFlags() {
+	flag.Usage = printUsage
+	flag.Parse()
+	// Remove flags and binary name from args.
+	args := []string{}
+	for _, arg := range os.Args[1:] {
+		if !strings.HasPrefix(arg, "-") {
+			args = append(args, arg)
 		}
+	}
+	if *clear && *global == "" && *unglobal == "" {
+		doClear(args)
+	} else if *global != "" && !(*clear) && *unglobal == "" {
+		doGlobal(args)
+	} else if *unglobal != "" && !(*clear) && *global == "" {
+		doUnglobal(args)
+	} else if *clear || *global != "" || *unglobal != "" {
+		fmt.Println("one flag at a time please")
+		os.Exit(1)
+	} else {
 		return
 	}
-	fmt.Println(path)
+	os.Exit(0)
 }
 
-func set(path string) {
-	clear()
-	err := os.Symlink(path, spacefile())
+func doClear(args []string) {
+	if len(args) != 0 {
+		fmt.Println("-clear doesn't take any args")
+		return
+	}
+	dst := "/dev/null"
+	link := spacefile()
+	err := setLink(link, dst)
 	if err != nil {
 		fmt.Println(err)
-		return
 	}
-	get()
+	fmt.Printf("%s -> %s\n", link, dst)
 }
 
-func clear() {
-	err := os.Remove(spacefile())
-	if err != nil {
-		if !os.IsNotExist(err) {
-			fmt.Println(err)
-		}
+func doGlobal(args []string) {
+	if len(args) != 1 {
+		fmt.Println("-global takes exactly 1 argument")
+		return
 	}
+	dst := args[0]
+	base := filepath.Base(dst)
+	link := "/usr/local/bin/" + base
+	err := setLink(link, dst)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%s -> %s\n", link, dst)
+}
+
+func doUnglobal(args []string) {
+	if len(args) != 1 {
+		fmt.Println("-unglobal takes exactly 1 argument")
+		return
+	}
+	dst := args[0]
+	base := filepath.Base(dst)
+	link := "/usr/local/bin/" + base
+	err := rmLink(link)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("removed %s\n", link)
+}
+
+func readLink(path string) (string, error) {
+	dst, err := os.Readlink(path)
+	if err != nil && os.IsNotExist(err) {
+		return "", nil
+	}
+	return dst, err
+}
+
+func setLink(name, dst string) error {
+	err := rmLink(name)
+	if err != nil {
+		return err
+	}
+	err = os.Symlink(dst, name)
+	return err
+}
+
+func rmLink(path string) error {
+	err := os.Remove(path)
+	if err != nil && !os.IsNotExist(err) {
+		return err
+	}
+	return nil
 }
 
 func spacefile() string {
